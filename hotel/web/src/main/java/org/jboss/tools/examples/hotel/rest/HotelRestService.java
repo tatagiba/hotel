89df5402847928da63e59258607b7c825199f0e7
tata <tata@tata-Vostro-3360>
2017-04-29 00:02:00 -0300

    Início do projeto

---
+++ hotel/web/src/main/java/org/jboss/tools/examples/hotel/rest/HotelRestService.java
@@ -0,0 +1,20 @@
+package org.jboss.tools.examples.hotel.rest;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/cliente/{cpf:[0-9][0-9]*}")
+@RequestScoped
+public class HotelRestService {
+	
+	@GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String ConsultaCliente(@PathParam("cpf")int cpf) {
+        return "Cliente Cliente "+ cpf;
+    }
+
+}

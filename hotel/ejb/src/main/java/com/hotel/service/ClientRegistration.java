041933febdf7b5c9b5b90b1b15aa16125ff0c0d8
tata <tata@tata-Vostro-3360>
2017-06-09 23:35:05 -0300

    Logging

--- hotel/ejb/src/main/java/com/hotel/service/ClientRegistration.java
+++ hotel/ejb/src/main/java/com/hotel/service/ClientRegistration.java
@@ -2,12 +2,15 @@ package com.hotel.service;
 
 import java.util.List;
 
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+
+import org.apache.log4j.Logger;
 
 import com.hotel.model.Cliente;
 
@@ -18,9 +21,14 @@ public class ClientRegistration {
 	@PersistenceContext
 	private EntityManager em;
 	
+	private static final Logger LOGGER = Logger.getLogger(ClientRegistration.class);
+	
+	
 	public List<Cliente> todosOsClientes(){
 		Query query = em.createNativeQuery("Select * from cliente", Cliente.class);
 		List<Cliente> resultado = query.getResultList();		
+	
+		LOGGER.info("Vai voltar");
 		return resultado;
 		
 	}
